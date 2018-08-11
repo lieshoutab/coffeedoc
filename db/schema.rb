724849566fa19fc099ace2cfb6c794eecb9254ba
Rails Bridge <railsbridge@Railss-MacBook-Pro.local>
2018-08-12 11:53:29 +1200

    photos model, routes for photos, added photo controller, views

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_225048) do
+ActiveRecord::Schema.define(version: 2018_08_11_231016) do
 
   create_table "drinks", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.string "url"
+    t.integer "drink_id"
+    t.index ["drink_id"], name: "index_photos_on_drink_id"
   end
 
 end
