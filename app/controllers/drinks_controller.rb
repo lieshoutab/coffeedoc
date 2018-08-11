10ae32d00801790a2df38dc7d219ff2b9e27c649
Grace <d1v1nagrace@yahoo.com>
2018-08-12 11:29:40 +1200

    Updated views to create a new coffee

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -14,4 +14,20 @@ class DrinksController < ApplicationController
   def new
     @drink = Drink.new
   end
+
+def create
+  @drink = Drink.new(drink_params)
+
+
+      if @drink.save
+        redirect_to drinks_path, notice: 'Coffee was successfully created.'
+      end
+end
+
+
+private
+def drink_params
+  params.require(:drink).permit(:name)
+end
+
 end
